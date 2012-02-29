025f860bda19fa3b7236be9083470bdd8fb207d8
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-02-28 21:28:42 -0800

    Permissions support

--- PickupArrows.java
+++ PickupArrows.java
@@ -1,6 +1,7 @@
 package me.exphc.PickupArrows;
 
 import java.util.logging.Logger;
+import java.util.List;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.*;
@@ -24,25 +25,66 @@ class PickupArrowsListener implements Listener {
 	}
 
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onProjectileHit(ProjectileHitEvent event) {
-        Entity entity = event.getEntity();
-        if (!(entity instanceof Arrow)) {
-            return;
-        }
+	public void onEntityShootBow(EntityShootBowEvent event) {
+		Entity projectile = event.getProjectile();
+		if (!(projectile instanceof Arrow)) {
+			return;
+		}
+		Arrow arrow = (Arrow)projectile;
 
-        Arrow arrow = (Arrow)entity;
+		Entity shooter = event.getEntity();
+		if (shooter instanceof Skeleton || !plugin.getConfig().getBoolean("skeletonsOnly", true)) {
+			plugin.log("shot by "+shooter);
 
-        ((CraftArrow)arrow).getHandle().fromPlayer = true;
+            // anyone can pickup
+            if (!plugin.getConfig().getBoolean("usePermissions", false)) {
+                plugin.log("allowed by configuration");
+                setAllowPickup(arrow);
+                return;
+            }
+
+            // otherwise, check if anyone nearby is allowed
+			double r = plugin.getConfig().getDouble("range", 10.0);
+			List<Entity> nearbyEntities = shooter.getNearbyEntities(r, r, r);
+			for (Entity nearbyEntity: nearbyEntities) {
+				if (nearbyEntity instanceof Player) {
+					Player player = (Player)nearbyEntity;
+
+					if (player.hasPermission("pickuparrows.skeleton")) {
+						plugin.log("allowed by permissions of "+player.getName());
+                        setAllowPickup(arrow);
+						return;
+					} else {
+                        plugin.log("no permission from "+player.getName());
+                    }
+				}
+			}
+			plugin.log("not allowed");
+		}
 	}
+
+    public void setAllowPickup(Arrow arrow) {
+        ((CraftArrow)arrow).getHandle().fromPlayer = true;
+    }
 }
 
 public class PickupArrows extends JavaPlugin {
     Logger log = Logger.getLogger("Minecraft");
 
     public void onEnable() {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        reloadConfig();
+
 		new PickupArrowsListener(this);
     }
 
     public void onDisable() {
+    }
+
+    public void log(String message) {
+        if (getConfig().getBoolean("debug", false)) {
+            log.info(message);
+        }
     }
 }

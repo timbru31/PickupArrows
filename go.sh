4ef003de262977fe02537461a11b0d68e2acc072
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-02-28 21:37:39 -0800

    Add BSD license

--- go.sh
+++ go.sh
@@ -3,4 +3,4 @@ CLASSPATH=../craftbukkit-1.1-R4.jar javac *.java -Xlint:unchecked -Xlint:depreca
 rm -rf me 
 mkdir -p me/exphc/PickupArrows
 mv *.class me/exphc/PickupArrows
-jar cf PickupArrows.jar me/ *.yml *.java
+jar cf PickupArrows.jar me/ *.yml *.java LICENSE README.md

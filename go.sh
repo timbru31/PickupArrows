22aac1e5d97c6cf96091262036c1dcea61eb1fe9
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2012-08-11 14:11:15 +0200

    Changed: Updated for 1.3.1 LICENSE CHANGE! NEW AUTHOR

--- go.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -x
-CLASSPATH=../craftbukkit-1.2.5-R1.0.jar javac *.java -Xlint:unchecked -Xlint:deprecation
-rm -rf me 
-mkdir -p me/exphc/PickupArrows
-mv *.class me/exphc/PickupArrows
-jar cf PickupArrows.jar me/ *.yml *.java LICENSE README.md ChangeLog

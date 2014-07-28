a855fdc40e361933c39c1159103aff4ad35a2a96
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2014-07-28 22:33:20 +0200

    Changed: indent using spaces only

--- src/main/java/de/dustplanet/pickuparrows/PickupArrows.java
+++ src/main/java/de/dustplanet/pickuparrows/PickupArrows.java
@@ -29,41 +29,41 @@ public class PickupArrows extends JavaPlugin {
     protected WorldGuardPlugin wg;
 
     public void onDisable() {
-	// Make our list empty
-	regions.clear();
+        // Make our list empty
+        regions.clear();
     }
 
     public void onEnable() {
-	// Event
-	getServer().getPluginManager().registerEvents(new PickupArrowsListener(this), this);
-	config = getConfig();
-	// Add defaults and copy them
-	config.options().header("For help please either refer to the bukkit dev page:\nhttp://dev.bukkit.org/bukkit-plugins/pickuparrows/");
-	config.addDefault("usePermissions", false);
-	String[] temp = {"skeleton", "player", "dispenser"};
-	for (String s : temp) {
-	    config.addDefault("pickupFrom." + s + "range", 10.0);
-	    config.addDefault("pickupFrom." + s + "fire", true);
-	    config.addDefault("pickupFrom." + s + "normal", true);
-	}
-	config.addDefault("pickupFrom.unknown.range", 5.0);
-	config.addDefault("pickupFrom.unknown.fire", false);
-	config.addDefault("pickupFrom.unknown.normal", false);
-	config.addDefault("useWorldGuard", false);
-	config.addDefault("useListAsBlacklist", false);
-	config.addDefault("regions", new ArrayList<String>());
-	config.options().copyDefaults(true);
-	saveConfig();
-	// WorldGuard regions
-	useWorldGuard = config.getBoolean("useWorldGuard");
-	if (useWorldGuard) {
-	    blacklist = config.getBoolean("useListAsBlacklist");
-	    regions = config.getStringList("regions");
-	    // WorldGuard plugin
-	    Plugin worldGuard = getServer().getPluginManager().getPlugin("WorldGuard");
-	    if (worldGuard != null && worldGuard instanceof WorldGuardPlugin) {
-		wg = (WorldGuardPlugin) worldGuard;
-	    }
-	}
+        // Event
+        getServer().getPluginManager().registerEvents(new PickupArrowsListener(this), this);
+        config = getConfig();
+        // Add defaults and copy them
+        config.options().header("For help please either refer to the bukkit dev page:\nhttp://dev.bukkit.org/bukkit-plugins/pickuparrows/");
+        config.addDefault("usePermissions", false);
+        String[] temp = {"skeleton", "player", "dispenser"};
+        for (String s : temp) {
+            config.addDefault("pickupFrom." + s + "range", 10.0);
+            config.addDefault("pickupFrom." + s + "fire", true);
+            config.addDefault("pickupFrom." + s + "normal", true);
+        }
+        config.addDefault("pickupFrom.unknown.range", 5.0);
+        config.addDefault("pickupFrom.unknown.fire", false);
+        config.addDefault("pickupFrom.unknown.normal", false);
+        config.addDefault("useWorldGuard", false);
+        config.addDefault("useListAsBlacklist", false);
+        config.addDefault("regions", new ArrayList<String>());
+        config.options().copyDefaults(true);
+        saveConfig();
+        // WorldGuard regions
+        useWorldGuard = config.getBoolean("useWorldGuard");
+        if (useWorldGuard) {
+            blacklist = config.getBoolean("useListAsBlacklist");
+            regions = config.getStringList("regions");
+            // WorldGuard plugin
+            Plugin worldGuard = getServer().getPluginManager().getPlugin("WorldGuard");
+            if (worldGuard != null && worldGuard instanceof WorldGuardPlugin) {
+                wg = (WorldGuardPlugin) worldGuard;
+            }
+        }
     }
 }
--- src/main/java/de/dustplanet/pickuparrows/PickupArrowsListener.java
+++ src/main/java/de/dustplanet/pickuparrows/PickupArrowsListener.java
@@ -33,63 +33,63 @@ public class PickupArrowsListener implements Listener {
     private PickupArrows plugin;
 
     public PickupArrowsListener(PickupArrows instance) {
-	plugin = instance;
+        plugin = instance;
     }
 
     @SuppressWarnings("deprecation")
     @EventHandler
     public void onProjectileHitEvent(ProjectileHitEvent event) {
-	Projectile projectile = event.getEntity();
-	// Arrow?
-	if (projectile == null || !(projectile instanceof Arrow)) {
-	    return;
-	}
-	// Get data
-	Arrow arrow = (Arrow) projectile;
-	ProjectileSource shooter = projectile.getShooter();
-	boolean onFire = arrow.getFireTicks() > 0 ? true : false;
-	String shooterName = "unknown";
-	if (shooter instanceof Player) {
-	    shooterName = "player";
-	} else if (shooter instanceof BlockProjectileSource) {
-	    shooterName = ((BlockProjectileSource) shooter).getBlock().getType().name().toLowerCase();
-	} else if (shooter instanceof LivingEntity) {
-	    shooterName  = ((LivingEntity) shooter).getType().getName().toLowerCase();
-	}
-		
-	// Make WorldGuard check
-	if (plugin.useWorldGuard && plugin.wg != null) {
-	    ApplicableRegionSet regionList = plugin.wg.getRegionManager(arrow.getWorld()).getApplicableRegions(arrow.getLocation());
-	    // If we use a whitelist and no regions are here, cancel
-	    if (regionList.size() == 0 && !plugin.blacklist) {
-		return;
-	    }
-	    // Iterate through the regions
-	    for (ProtectedRegion region : regionList) {
-		String regionName = region.getId();
-		// Either it's on the blacklist or not on the whitelist --> cancel
-		if ((plugin.blacklist && plugin.regions.contains(regionName)) || !plugin.regions.contains(regionName)) {
-		    return;
-		}
-	    }
-	}
-	
-	// First deny it & then check if we can allow it again
-	setPickup(arrow, 0);
-	
-	// Check if shooterName is in config, otherwise fallback again
-	shooterName = plugin.config.contains("pickupFrom." + shooterName)? shooterName : "unkown";
+        Projectile projectile = event.getEntity();
+        // Arrow?
+        if (projectile == null || !(projectile instanceof Arrow)) {
+            return;
+        }
+        // Get data
+        Arrow arrow = (Arrow) projectile;
+        ProjectileSource shooter = projectile.getShooter();
+        boolean onFire = arrow.getFireTicks() > 0 ? true : false;
+        String shooterName = "unknown";
+        if (shooter instanceof Player) {
+            shooterName = "player";
+        } else if (shooter instanceof BlockProjectileSource) {
+            shooterName = ((BlockProjectileSource) shooter).getBlock().getType().name().toLowerCase();
+        } else if (shooter instanceof LivingEntity) {
+            shooterName  = ((LivingEntity) shooter).getType().getName().toLowerCase();
+        }
 
-	// New check for flexible configuration
-	if (plugin.config.getBoolean("pickupFrom." + shooterName + ".fire") && onFire) {
-	    if (!plugin.config.getBoolean("usePermission") || rangeCheck(arrow, shooterName, (shooterName + ".fire"))) {
-		setPickup(arrow, 1);
-	    }
-	} else if (plugin.config.getBoolean("pickupFrom." + shooterName + ".normal") && !onFire) {
-	    if (!plugin.config.getBoolean("usePermission") || rangeCheck(arrow, shooterName, (shooterName + ".normal"))) {
-		setPickup(arrow, 1);
-	    }
-	}
+        // Make WorldGuard check
+        if (plugin.useWorldGuard && plugin.wg != null) {
+            ApplicableRegionSet regionList = plugin.wg.getRegionManager(arrow.getWorld()).getApplicableRegions(arrow.getLocation());
+            // If we use a whitelist and no regions are here, cancel
+            if (regionList.size() == 0 && !plugin.blacklist) {
+                return;
+            }
+            // Iterate through the regions
+            for (ProtectedRegion region : regionList) {
+                String regionName = region.getId();
+                // Either it's on the blacklist or not on the whitelist --> cancel
+                if ((plugin.blacklist && plugin.regions.contains(regionName)) || !plugin.regions.contains(regionName)) {
+                    return;
+                }
+            }
+        }
+
+        // First deny it & then check if we can allow it again
+        setPickup(arrow, 0);
+
+        // Check if shooterName is in config, otherwise fallback again
+        shooterName = plugin.config.contains("pickupFrom." + shooterName)? shooterName : "unkown";
+
+        // New check for flexible configuration
+        if (plugin.config.getBoolean("pickupFrom." + shooterName + ".fire") && onFire) {
+            if (!plugin.config.getBoolean("usePermission") || rangeCheck(arrow, shooterName, (shooterName + ".fire"))) {
+                setPickup(arrow, 1);
+            }
+        } else if (plugin.config.getBoolean("pickupFrom." + shooterName + ".normal") && !onFire) {
+            if (!plugin.config.getBoolean("usePermission") || rangeCheck(arrow, shooterName, (shooterName + ".normal"))) {
+                setPickup(arrow, 1);
+            }
+        }
     }
 
     /* 
@@ -98,26 +98,26 @@ public class PickupArrowsListener implements Listener {
      * 1 = enabled
      */
     private void setPickup(Arrow arrow, int i) {
-	((CraftArrow)arrow).getHandle().fromPlayer = i;
+        ((CraftArrow)arrow).getHandle().fromPlayer = i;
     }
 
     private boolean rangeCheck(Arrow arrow, String rangeSuffix, String permSuffix) {
-	// Get the range
-	double r = plugin.config.getDouble("range." + rangeSuffix , 10.0);
-	// Check for near entities
-	List<Entity> nearbyEntities = arrow.getNearbyEntities(r, r, r);
-	for (Entity nearbyEntity: nearbyEntities) {
-	    // Player found
-	    if (nearbyEntity instanceof Player) {
-		Player player = (Player) nearbyEntity;
-		// Check his permission
-		if (player.hasPermission("pickuparrows.allow." + permSuffix) || player.hasPermission("pickuparrows.allow.*")) {
-		    return true;
-		} else {
-		    return false;
-		}
-	    }
-	}
-	return false;
+        // Get the range
+        double r = plugin.config.getDouble("range." + rangeSuffix , 10.0);
+        // Check for near entities
+        List<Entity> nearbyEntities = arrow.getNearbyEntities(r, r, r);
+        for (Entity nearbyEntity: nearbyEntities) {
+            // Player found
+            if (nearbyEntity instanceof Player) {
+                Player player = (Player) nearbyEntity;
+                // Check his permission
+                if (player.hasPermission("pickuparrows.allow." + permSuffix) || player.hasPermission("pickuparrows.allow.*")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 }
